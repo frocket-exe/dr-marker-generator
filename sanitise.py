a15b0593d45731465d32e0ccc46604746fa46c92
frocket <freddie.rockett@gmail.com>
2026-08-21 17:03:05 +0100

    Made sanitisation work for multi-word line srt

--- sanitise.py
+++ sanitise.py
@@ -2,7 +2,7 @@ import re
 
 keepCap = ["I", "Kohl", "I'm", "I've", "I'd"]
 puncToRemove = r'[.!",;]'
-unsanitisedPath = "CLEAN_AUDIO.srt"
+unsanitisedPath = "proof_of_concept.srt"
 
 with open(unsanitisedPath) as f:
     file = f.read()
@@ -12,11 +12,15 @@ lines = file.split("\n\n")
 newLines = ""
 for line in lines:
     content = line.split("\n")[2]
-    if content.capitalize() in keepCap:
-        content = content.capitalize()
-    else:
-        content = content.lower()
     content = re.sub(puncToRemove, '', content)
+    content = content.split(" ")
+    words = []
+    for word in content:
+        if word.capitalize() in keepCap:
+            words.append(word.capitalize())
+        else:
+            words.append(word.lower())
+    content = " ".join(words)
     line = f"{line.split("\n")[0]}\n{line.split("\n")[1]}\n{content}"
     newLines += (line)
     newLines += ("\n\n")
